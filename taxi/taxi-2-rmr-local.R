c09f248f4ea1f1e103d5f3b2f85a9d064884241f
Andrie de Vries <apdevries@gmail.com>
2015-06-21 11:33:54 +0100

    Added analysis by hour

--- taxi/taxi-2-rmr-local.R
+++ taxi/taxi-2-rmr-local.R
@@ -55,7 +55,7 @@ x <- mapreduce(taxi.hdp, input.format = taxi.format,
                map = function(k, v){
                    original <- v[[6]]
                    date <- as.Date(original, origin = "1970-01-01")
-                   wkday = weekdays(date)
+                   wkday <- weekdays(date)
                    keyval(wkday, 1)
                })
 head(
@@ -72,7 +72,7 @@ x <- mapreduce(taxi.hdp, input.format = taxi.format,
                map = function(k, v){
                    original <- v[[6]]
                    date <- as.Date(original, origin = "1970-01-01")
-                   wkday = weekdays(date)
+                   wkday <- weekdays(date)
                    keyval(wkday, 1)
                },
                reduce = function(k, v){
@@ -90,7 +90,7 @@ x <- mapreduce(taxi.hdp, input.format = taxi.format,
                map = function(k, v){
                    original <- v[[6]]
                    date <- as.Date(original, origin = "1970-01-01")
-                   wkday = weekdays(date)
+                   wkday <- weekdays(date)
                    dat <- data.frame(date, wkday)
                    z <- aggregate(date ~ wkday, dat, FUN = length)
                    keyval(z[[1]], z[[2]])
@@ -103,14 +103,47 @@ x <- mapreduce(taxi.hdp, input.format = taxi.format,
                map = function(k, v){
                    original <- v[[6]]
                    date <- as.Date(original, origin = "1970-01-01")
-                   wkday = weekdays(date)
+                   wkday <- weekdays(date)
                    dat <- data.frame(date, wkday)
                    z <- aggregate(date ~ wkday, dat, FUN = length)
                    keyval(z[[1]], z[[2]])
                },
                reduce = function(k, v){
-                   keyval(k, sum(v))
+                   data.frame(hour = k, trips = sum(v), row.names = k)
                })
 keys(from.dfs(x))
 values(from.dfs(x))
 
+## @knitr mapreduce-7-a ---------------------------------------------------
+taxi.map <- function(k, v){
+    original <- v[[6]]
+    date <- as.Date(original, origin = "1970-01-01")
+    wkday <- weekdays(date)
+    hour <- format(as.POSIXct(original), "%H")
+    dat <- data.frame(date, hour)
+    z <- aggregate(date ~ hour, dat, FUN = length)
+    keyval(z[[1]], z[[2]])
+}
+
+taxi.reduce <- function(k, v){
+    data.frame(hour = k, trips = sum(v), row.names = k)
+}
+
+## @knitr mapreduce-7-b ---------------------------------------------------
+x <- mapreduce(taxi.hdp, input.format = taxi.format,
+               map = taxi.map,
+               reduce = taxi.reduce
+               )
+keys(from.dfs(x))
+dat <- values(from.dfs(x))
+dat
+
+## @knitr mapreduce-7-plot ------------------------------------------------
+
+
+library(ggplot2)
+ggplot(dat, aes(x = hour, y = trips, group = 1)) +
+    geom_line(col = "blue") +
+    expand_limits(y = 0) +
+    geom_smooth(method = loess, span = 0.5, alpha = 0.2, col = "grey50") +
+    ggtitle("Sample of taxi trips in New York")
